bf3e90a0e086b0fcf5ba3573613565087f4105eb
孟庆 <670608607@qq.com>
2018-03-26 17:03:48 +0800

    啊啊啊啊

---
+++ baizhi-cmfz-sys/src/main/java/com/baizhi/test/Test.java
@@ -0,0 +1,12 @@
+package com.baizhi.test;
+
+/**
+ * Created with mengqing.
+ * Date: 2018/3/26 0026
+ * Time: 下午 4:28
+ * 职位增删改查. 该类作用
+ */
+public class Test {
+    public static void main(String[] args) {
+    }
+}
